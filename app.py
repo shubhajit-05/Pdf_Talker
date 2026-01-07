3faec44c4d2cc41a595bd629155363591326338c
shubhajit-05 <shubhajitshaw5@gmail.com>
2026-01-08 00:07:18 +0530

    Fix RetrievalQA import for new LangChain

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.chains import RetrievalQA
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 def main():
